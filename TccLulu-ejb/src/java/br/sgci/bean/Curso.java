38475246a8164a903f54c1ac1dbaf068b96c6fa9
Luisiane Pereira Costa Pinto <lulucostapinto@gmail.com>
2014-11-03 22:29:44 -0200

    2

--- TccLulu-ejb/src/java/br/sgci/bean/Curso.java
+++ TccLulu-ejb/src/java/br/sgci/bean/Curso.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +10,6 @@ package br.sgci.bean;
 
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
